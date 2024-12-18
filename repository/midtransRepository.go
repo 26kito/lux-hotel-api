3e2a3dce267a945dac5b79f29d1526db70dbb6c8
kito <surya.aw26@gmail.com>
2024-12-18 22:07:29 +0700

    feat: handle midtrans callback for booking room

--- repository/midtransRepository.go
+++ repository/midtransRepository.go
@@ -8,6 +8,7 @@ import (
 
 type MidtransRepository interface {
 	HandleTopUpCallback(entity.MidtransCallbackResponse)
+	HandleBookingCallback(entity.MidtransCallbackResponse)
 }
 
 type midtransRepository struct {
@@ -38,6 +39,30 @@ func (mr *midtransRepository) HandleTopUpCallback(payload entity.MidtransCallbac
 		var payment entity.Payment
 
 		mr.DB.Model(&transaction).Where("order_id = ?", payload.OrderID).Update("transaction_status", payload.TransactionStatus)
+		mr.DB.Model(&payment).Where("order_id = ?", payload.OrderID).Update("payment_status", payload.TransactionStatus)
+	}
+}
+
+func (mr *midtransRepository) HandleBookingCallback(payload entity.MidtransCallbackResponse) {
+	if payload.TransactionStatus == "settlement" {
+		var booking entity.Booking
+		var payment entity.Payment
+		var room entity.Room
+
+		mr.DB.Where("order_id = ?", payload.OrderID).First(&booking)
+		mr.DB.Model(&room).Where("hotel_id = ? AND id = ?", booking.HotelID, booking.RoomID).Update("status", "occupied")
+		mr.DB.Model(&booking).Update("booking_status", "settlement")
+		mr.DB.Model(&payment).Where("order_id = ?", payload.OrderID).Updates(map[string]interface{}{
+			"payment_status": "settlement",
+			"payment_date":   payload.TransactionTime,
+		})
+	}
+
+	if payload.TransactionStatus == "expire" || payload.TransactionStatus == "cancel" {
+		var booking entity.Booking
+		var payment entity.Payment
+
+		mr.DB.Model(&booking).Where("order_id = ?", payload.OrderID).Update("booking_status", payload.TransactionStatus)
 		mr.DB.Model(&payment).Where("order_id = ?", payload.OrderID).Update("payment_status", payload.TransactionStatus)
 	}
 }
--- service/midtransService.go
+++ service/midtransService.go
@@ -34,6 +34,10 @@ func (ms *midtransService) HandleMidtransCallback(c echo.Context) error {
 	// Identify transaction by order_id
 	if strings.Contains(payload.OrderID, "TPUP") {
 		ms.MidtransRepository.HandleTopUpCallback(payload)
+	} else if strings.Contains(payload.OrderID, "BKNG") {
+		ms.MidtransRepository.HandleBookingCallback(payload)
+	} else {
+		return c.JSON(http.StatusBadRequest, map[string]string{"message": "Invalid order_id"})
 	}
 
 	return c.JSON(http.StatusOK, map[string]string{"message": "Callback processed"})
